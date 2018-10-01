5e690224fb251651fac19460820948def588f004
Niall Eccles <n.eccles14@gmail.com>
2018-10-01 21:22:14 +0100

    Initial commit.

---
+++ script.js
@@ -0,0 +1,15 @@
+function getAnimationName() {
+  const e = document.querySelector("#target");
+  const select = document.querySelector("#animationSelect");
+  e.className = 'raleway ';
+  let value = select.options[select.selectedIndex].value;
+  let text = select.options[select.selectedIndex].text;
+  let dataLink = select.options[select.selectedIndex].dataset.link;
+  let strArray = text.split('-');
+  let animationName = document.querySelector('#animationName');
+  let userName = document.querySelector('#userName');
+  animationName.textContent = strArray[0];
+  userName.textContent = strArray[1];
+  userName.href = dataLink;
+  e.classList.add(value);
+}
