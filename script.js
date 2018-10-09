5f3f444752d781cbe774e7600c0f393e745a7ca2
adsingh14 <deep.amansingh14@gmail.com>
2018-10-10 02:03:21 +0530

    Added 2 effects

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function getAnimationName() {
 
     const e = document.querySelector("#target");
     const select = document.querySelector("#animationSelect");
-    shuffle(select);
     e.className = "title";
     let value = select.options[select.selectedIndex].value;
     let text = select.options[select.selectedIndex].text;
@@ -18,21 +17,4 @@ function getAnimationName() {
     userName.textContent = strArray[1];
     userName.href = dataLink;
     e.classList.add(value);
-}
-
-
-// @description Shuffle function by Fisher-Yates (aka Knuth-shuffle).
-function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue, randomIndex;
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
 }
