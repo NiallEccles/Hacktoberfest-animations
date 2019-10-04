85e3421569cbb01e0d96169e1e956783d83a3e5b
Fernando Santos <fern@ndo.io>
2019-10-03 23:14:31 -0300

    Revert "Random animation on Load/Refresh (closes #60)"
    This reverts commit 1397d7b0b09ef609044cd0cd6b49373ec61f1a3f.

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 "use strict";
-window.onload = getAnimationName(true);
+window.onload = getAnimationName();
 
-function getAnimationName(rand) {
-  rand = rand || false;
+function getAnimationName() {
   document.getElementById("detail_elements").style.visibility = "visible";
 
   const e = document.querySelector("#target");
   const select = document.querySelector("#animationSelect");
-  const index = (rand) ? Math.floor(Math.random() * (select.length + 1)) : select.selectedIndex;
   e.className = "title";
-  let value = select.options[index].value;
-  let text = select.options[index].text;
-  let dataLink = select.options[index].dataset.link;
+  let value = select.options[select.selectedIndex].value;
+  let text = select.options[select.selectedIndex].text;
+  let dataLink = select.options[select.selectedIndex].dataset.link;
   let strArray = text.split("-");
   let animationName = document.querySelector("#animationName");
   let userName = document.querySelector("#userName");
@@ -19,8 +17,4 @@ function getAnimationName(rand) {
   userName.textContent = strArray[1];
   userName.href = dataLink;
   e.classList.add(value);
-  // If rand is true, also change the selected value on the select
-  if(rand){
-    select.selectedIndex = index;
-  }
 }
