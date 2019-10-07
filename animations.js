18295eb57218ba662806354acdc9b9ec4906a2e9
Afraz Khan <khan.afraz4183@yahoo.com>
2019-10-07 21:08:02 +0500

    Created new animation
    Made new animation as 'rotate-it' by classname.

--- animations.js
+++ animations.js
@@ -94,5 +94,6 @@ window.animations = [
   { name: 'zoomIn', author: 'MansiNegi27' },
   { name: 'zoomright', author: 'sunny-aguilar' },
   { name: 'zoomInLeft', author: 'fernandosutter' },
+  { name: 'rotate-it', author: 'MrAfrazKhan' },
 
 ]
