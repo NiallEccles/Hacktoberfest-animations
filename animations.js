d2ca6718bb5742dd5df291f714d8e1d0f3863e0c
Somechandra Sen <somechandra.sen@CaprusIT.local>
2019-10-10 11:37:04 +0530

    added coloreffect animation

--- animations.js
+++ animations.js
@@ -109,5 +109,6 @@ window.animations = [
   { name: 'wobble', author: 'golomann'},
   { name: 'zoomIn', author: 'MansiNegi27' },
   { name: 'zoomright', author: 'sunny-aguilar' },
-  { name: 'zoomInLeft', author: 'fernandosutter' }
+  { name: 'zoomInLeft', author: 'fernandosutter' },
+  { name: 'coloreffect', author: 'somechandra' }
 ]
