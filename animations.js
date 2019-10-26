487f62480b174faec37e8b492bb83b8de5f6c31d
unknown <fernades.bianca2005@hotmail.com>
2019-10-26 19:34:30 -0300

    Named the animation in animations.js

--- animations.js
+++ animations.js
@@ -1,6 +1,7 @@
 /** @format */
 
 window.animations = [
+    { name: 'FadeOutLeft', author: 'Biafer'},
 	{ name: 'aberration', author: 'cnsumner' },
 	{ name: 'autumn-gradient', author: 'marichka-offen' },
 	{ name: 'backFlip', author: 'adsingh14' },
