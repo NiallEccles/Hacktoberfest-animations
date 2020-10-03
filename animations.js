04f2e5b179aeb4d9e9f7738b1ec4febba9b49b59
Adriano Avelino <adriano-st777@hotmail.com>
2020-10-03 17:17:45 -0300

    add slideDownWithBorder animation

--- animations.js
+++ animations.js
@@ -1,6 +1,7 @@
 /** @format */
 
 window.animations = [
+	{ name: 'slideDownWithBorder', author: 'adrianoavelino' },
 	{ name: 'staircase_bounce', author: 'vivekrawat' },
 	{ name: 'bounce', author: 'Biafer' },
 	{ name: 'fadeOutLeft', author: 'Biafer' },
