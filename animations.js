71d7fe642efc982f371a45021e04805cbbec1450
Akansha-Bhandari <56572942+Akansha-Bhandari@users.noreply.github.com>
2020-10-07 18:00:29 +0530

    Update animations.js

--- animations.js
+++ animations.js
@@ -185,10 +185,10 @@ window.animations = [
 	{ name: 'lr-loop', author: 'luca-gouveia' },
 	{ name: 'thaovt', author: 'thaovt' },
 	{ name: 'bubbles', author: 'Akshama' },
-        { name: 'Invicta-2', author: 'dhruvkumar4567' },
-        { name: 'Invict', author: 'mani9896' },
-        { name: 'lr-loop', author: 'Shivang0' },
-        { name: 'popping' , author: 'Akansha-Bhandari'}
+  { name: 'Invicta-2', author: 'dhruvkumar4567' },
+  { name: 'Invict', author: 'mani9896' },
+  { name: 'lr-loop', author: 'Shivang0' },
+  { name: 'popping' , author: 'Akansha-Bhandari'}
         
 
 ];
