3412e94c3417785f2e17beff0437e8a30073cc94
muskan-9 <muskanjain792@gmail.com>
2020-10-16 15:59:03 +0530

    Added an animation

--- animations.js
+++ animations.js
@@ -229,4 +229,5 @@ window.animations = [
 	{ name: 'glowing', author: 'brunoeduardodev' },
 	{ name: 'cardy', author: 'antonioleutsch' },
 	{ name: 'save-amazon', author: 'claudioholanda' },
+	{ name: 'glow-sign', author: 'muskan-9'},
 ];
