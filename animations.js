86d75a0e10a4fc567c753066e8fd6c2a456948f2
aflajismail <58683155+aflajismail@users.noreply.github.com>
2020-10-19 03:24:27 +0530

    Update animations.js

--- animations.js
+++ animations.js
@@ -241,4 +241,5 @@ window.animations = [
 	{ name: 'glowing', author: 'brunoeduardodev' },
 	{ name: 'cardy', author: 'antonioleutsch' },
 	{ name: 'save-amazon', author: 'claudioholanda' },
+  { name: 'colorfull', author: 'aflajismail' },
 ];
