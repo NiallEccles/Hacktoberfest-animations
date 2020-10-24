d93016a8fc9b701ee7dd6f56a65f8d116ee0b079
ledangtuanbk <ledangtuanbk@users.noreply.github.com>
2020-10-24 08:00:12 +0700

    Add animation and username

--- animations.js
+++ animations.js
@@ -253,4 +253,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+  { name: 'colourfull-ldt2020', author: 'ledangtuanbk' },
 ];
