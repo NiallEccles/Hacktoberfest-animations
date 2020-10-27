21253b89dfa422557fa02a17c9a68c7fab33e999
Pliavi <pliavi.rpgm@gmail.com>
2020-10-27 12:55:20 -0300

    add pixar lamp animation with H letter

--- animations.js
+++ animations.js
@@ -167,6 +167,7 @@ window.animations = [
 	{ name: 'pinheiro', author: 'micaelgoms' },
 	{ name: 'piston', author: 'sockopera' },
 	{ name: 'pivot', author: 'ronlaniado' },
+	{ name: 'pixar-h', author: 'pliavi' },
 	{ name: 'popping', author: 'Akansha-Bhandari' },
 	{ name: 'press-start', author: 'MagoMagoado' },
 	{ name: 'pudding', author: 'sliceolife' },
