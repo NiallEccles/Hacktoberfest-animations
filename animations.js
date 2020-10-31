9248eeda4540a25b2942e60166c703f98579b74e
manisha <manisha_kanyal.scsebtech@galgotiasuniversity.edu.in>
2020-10-31 17:34:36 +0530

    css added

--- animations.js
+++ animations.js
@@ -276,4 +276,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+	{name: 'minihimiash', author: 'michiiplusplus'}
 ];
