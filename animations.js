30051a32bdf2580497a242630289fcdf6808437b
Joseph D'Agostino <Joseph.D'Agostino@stockton.edu>
2021-10-11 15:32:00 -0400

    added bounce-simple animation

--- animations.js
+++ animations.js
@@ -288,4 +288,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+	{ name: 'bounce-simple', author: 'joedag32' }
 ];
