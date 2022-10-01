e5c32376800367c458ab63e550ad58b423856210
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2022-10-01 13:45:02 +0530

    Updated Animations.js

--- animations.js
+++ animations.js
@@ -322,4 +322,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+  { name: 'shaking', author: 'shaking'},
 ];
