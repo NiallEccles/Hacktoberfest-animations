4d537f3c9e29d3a4547a17a938a5b7c26a32183e
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2022-10-03 22:59:33 +0530

    Edited The Name In animations.js

--- animations.js
+++ animations.js
@@ -330,4 +330,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+  { name: 'changingTextShadow', author: 'code08-ind' }
 ];
