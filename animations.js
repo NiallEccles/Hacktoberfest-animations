4545147143af88dcce7f697d7341185b3e59e7b0
Gabriel Vitebo <vitebo@outlook.com>
2022-10-15 15:17:58 -0300

    adding a new animation

--- animations.js
+++ animations.js
@@ -399,4 +399,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+	{name: 'comes-and-goes', author: 'gabriel-vitebo'}
 ];
