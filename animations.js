e53f3aca5579bd8211196cc3f3bb0c6baab2fbec
frederic <fred@Inspiron-One-2310-c833fd70.localdomain>
2022-10-15 17:05:59 +0200

    my first animation

--- animations.js
+++ animations.js
@@ -399,4 +399,5 @@ window.animations = [
 	{ name: 'zoomInLeft', author: 'fernandosutter' },
 	{ name: 'zoomOut', author: 'Biafer' },
 	{ name: 'zoomright', author: 'sunny-aguilar' },
+	{ name: 'zebra', author: 'flagage' },
 ];
