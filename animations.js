3030fc7b85c221f1edc149d0ca0eb203053b2285
Kumar Ashish Ranjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2022-10-16 22:58:41 +0530

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,6 +1,7 @@
 /** @format */
 
 window.animations = [
+	{ name: 'ashishMotion', author: 'dev-AshishRanjan' },
 	{ name: 'Animadeu', author: 'Amad3eu' },
 	{ name: 'Anubhav-developerMation', author: 'Anubhav-developr' },
 	{ name: 'aberration', author: 'cnsumner' },
