33fda7238069e69fd9e731cc9e80d9883b66928f
Niall Eccles <n.eccles14@gmail.com>
2022-10-22 18:43:36 +0100

    Update animations.js

--- animations.js
+++ animations.js
@@ -428,5 +428,11 @@ window.animations = [
 	{ name: 'comes-and-goes', author: 'gabriel-vitebo' },
 	{ name: 'on-fire', author: 'Giulia Amaral'},
 	{ name: 'manvi2912', author: 'manvi2912' },
-  { name: 'flip-text-animation', author: 'pggeeks' },
+	{ name: 'aaodev', author: 'rodrigoodev' },
+	{ name: 'stroke-slide', author: 'Robert-Capra' },
+	{ name: 'running-letters', author: 'avinishrao' },
+	{ name: 'zebra', author: 'avinishrao' },
+	{ name: 'danceit', author: 'Dev-Ahmadubah' },
+	{ name: 'bouncy', author: 'AmanPathan' },
+	{ name: 'flip-text-animation', author: 'pggeeks' },
 ];
