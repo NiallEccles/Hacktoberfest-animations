e7e553bc1f451c149335b3775c0c68fa578568d9
Raissa Cardoso <raissa_cardoso@yahoo.com.br>
2022-10-28 18:03:51 -0300

    add crazyText animation

--- animations.js
+++ animations.js
@@ -454,4 +454,5 @@ window.animations = [
 	{ name: 'halloween-dance', author: 'LindseaMartin' },
 	{ name: 'bounceOutRight', author: 'katsu53' },
 	{ name: 'flipText', author: '204G1A0542' },
+	{ name: 'crazyText', author: 'Raissa-Cardoso'},
 ];
